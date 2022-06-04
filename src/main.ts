ff487d5e540ace71b7b86950ec802b13bf119978
Charles Montgomery <cm2jr4@gmail.com>
2022-06-04 16:56:06 +0900

    testing pug plugin - 2022/06/04

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,6 @@
 import "./style.scss";
 
-import about from "../data/about.json";
-import featuredWork from "../data/featured.json";
-import medias from "../data/medias.json";
-
 const app = document.querySelector<HTMLDivElement>("#app")!;
-
-console.log(about);
-console.log(featuredWork);
-console.log(medias);
 
 app.innerHTML = `
   <h1>Hello Vite!</h1>
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import pugPlugin from "vite-plugin-pug";
+
+import about from "./data/about.json";
+import featuredWork from "./data/featured.json";
+import medias from "./data/medias.json";
+
+const options = {};
+const locals = { name: "My Pug", medias, featuredWork, about };
+
+export default defineConfig({
+  plugins: [pugPlugin(options, locals)],
+});
